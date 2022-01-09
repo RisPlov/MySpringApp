d2b7363938d1273455b58c40b9c14038b1439c15
Admin <slava.aristov@bk.ru>
2022-01-09 21:38:48 +0500

    Third commit

---
+++ src/main/java/test/example/JazMusic.java
@@ -0,0 +1,9 @@
+package test.example;
+
+public class JazMusic implements Music{
+
+    @Override
+    public String getSong() {
+        return "My way";
+    }
+}
--- src/main/java/test/example/TestSpring.java
+++ src/main/java/test/example/TestSpring.java
@@ -7,10 +7,10 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "ApplicationContext.xml"
         );
-        Music music = context.getBean("musicBean", Music.class);
+        //Music music = context.getBean("musicBean", Music.class);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
+        //MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
         context.close();
